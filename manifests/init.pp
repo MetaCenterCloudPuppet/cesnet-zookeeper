80ddf903dd4e43500ea8a4c0b36afe34c7e98ec4
František Dvořák <valtri@civ.zcu.cz>
2017-11-14 18:34:37 +0100

    Regex boundaries in auth_to_local rules are not needed

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,15 +30,15 @@ class zookeeper (
     $sec_properties = {
       'java.security.auth.login.config' => "${zookeeper::confdir}/jaas.conf",
       'zookeeper.security.auth_to_local' => "
-RULE:[2:\$1;\$2@\$0](^jhs;.*@${realm}$)s/^.*$/mapred/
-RULE:[2:\$1;\$2@\$0](^[ndjs]n;.*@${realm}$)s/^.*$/hdfs/
-RULE:[2:\$1;\$2@\$0](^[rn]m;.*@${realm}$)s/^.*$/yarn/
-RULE:[2:\$1;\$2@\$0](^hbase;.*@${realm}$)s/^.*$/hbase/
-RULE:[2:\$1;\$2@\$0](^hive;.*@${realm}$)s/^.*$/hive/
-RULE:[2:\$1;\$2@\$0](^hue;.*@${realm}$)s/^.*$/hue/
-RULE:[2:\$1;\$2@\$0](^tomcat;.*@${realm}$)s/^.*$/tomcat/
-RULE:[2:\$1;\$2@\$0](^zookeeper;.*@${realm}$)s/^.*$/zookeeper/
-RULE:[2:\$1;\$2@\$0](^HTTP;.*@${realm}$)s/^.*$/HTTP/
+RULE:[2:\$1;\$2@\$0](jhs;.*@${realm})s/^.*$/mapred/
+RULE:[2:\$1;\$2@\$0]([ndjs]n;.*@${realm})s/^.*$/hdfs/
+RULE:[2:\$1;\$2@\$0]([rn]m;.*@${realm})s/^.*$/yarn/
+RULE:[2:\$1;\$2@\$0](hbase;.*@${realm})s/^.*$/hbase/
+RULE:[2:\$1;\$2@\$0](hive;.*@${realm})s/^.*$/hive/
+RULE:[2:\$1;\$2@\$0](hue;.*@${realm})s/^.*$/hue/
+RULE:[2:\$1;\$2@\$0](tomcat;.*@${realm})s/^.*$/tomcat/
+RULE:[2:\$1;\$2@\$0](zookeeper;.*@${realm})s/^.*$/zookeeper/
+RULE:[2:\$1;\$2@\$0](HTTP;.*@${realm})s/^.*$/HTTP/
 DEFAULT
 ",
     }
